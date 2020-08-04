241dda26304b865182d21548d584fc0390a0ed8f
anhnguyenphung <anhnguyenphung@email,arizona,edu>
2020-08-03 23:35:36 -0700

    Add images and make changes to models.py

--- django_project/models.py
+++ django_project/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Django_Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
+
+    def __str__(self):
+        return self.title
+
